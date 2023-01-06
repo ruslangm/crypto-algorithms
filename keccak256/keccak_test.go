98a802e48bd288af7ef4560f73536e3ece034083
Ruslan <ruslan.gmz@gmail.com>
2023-01-06 13:17:28 +0400

    added test vectors

--- keccak256/keccak_test.go
+++ keccak256/keccak_test.go
@@ -2,17 +2,22 @@ package keccak
 
 import (
 	"bytes"
-	"github.com/ethereum/go-ethereum/common"
 	"testing"
 )
 
-func TestKeccak256(t *testing.T) {
+type testcase struct {
+	msg    []byte
+	output []byte
+}
+
+func TestKeccakShort256(t *testing.T) {
 	h := newKeccak256()
-	h.Reset()
-	h.Write([]byte("The quick brown fox jumps over the lazy dog, The quick brown fox jumps over the lazy dog, The quick brown fox jumps over the lazy dog, The quick brown fox jumps over the lazy dog, The quick brown fox jumps over the lazy dog, The quick brown fox jumps over the lazy dog"))
-	d := h.Sum(nil)
-	expected := "e4b5f1d230b0f84b4c7261a7b078cef6d3a7af18256f58d6f817658fd7bc08a2"
-	if !bytes.Equal(d, common.Hex2Bytes(expected)) {
-		t.Errorf("Testcase Keccak256 failed, expected %x got %x", expected, d)
+	for i := range tstShort {
+		h.Reset()
+		h.Write(tstShort[i].msg)
+		d := h.Sum(nil)
+		if !bytes.Equal(d, tstShort[i].output) {
+			t.Errorf("testcase Short256 %d: expected %x got %x", i, tstShort[i].output, d)
+		}
 	}
 }
